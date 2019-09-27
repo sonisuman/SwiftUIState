c8bb6258633b1aab6a9e7b0f433174c53527ec3c
Soni suman <soni.suman@impelsys.com>
2019-09-27 14:48:59 +0530

    Initial Commit

---
+++ SwiftUIStateSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStateSample
+//
+//  Created by Soni Suman on 9/27/19.
+//  Copyright © 2019 Soni Suman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
